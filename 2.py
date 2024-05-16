9303545426ed0081d6edd815932f7042317f8d6f
Blevotin <dima.dirys@mail.ru>
2024-05-16 19:57:09 +0300

    2.py

--- 2.py
+++ 2.py
@@ -1,20 +1,20 @@
-# price_of_the_product = 0
-# product_weight = 0
-# change = 0
-# print("Введите имя товара плз")
-# Product_Name = input()
-# print("Введите цену товара в Руб\Кг плз")
-# price_of_the_product = int(input())
-# print("Введите вес :) товара в Кг плз")
-# product_weight = int(input())
-# print(Product_Name, product_weight, 'Кг', price_of_the_product, 'Руб/кг')
-# print("Введите ваш бюджет плз")
-# money = int(input())
-# print('Итоговая стоимость ', product_weight * price_of_the_product, 'Бюджет', money, 'Сдача', money - product_weight * price_of_the_product)
+price_of_the_product = 0
+product_weight = 0
+change = 0
+print("Введите имя товара плз")
+Product_Name = input()
+print("Введите цену товара в Руб\Кг плз")
+price_of_the_product = int(input())
+print("Введите вес :) товара в Кг плз")
+product_weight = int(input())
+print(Product_Name, product_weight, 'Кг', price_of_the_product, 'Руб/кг')
+print("Введите ваш бюджет плз")
+money = int(input())
+print('Итоговая стоимость ', product_weight * price_of_the_product, 'Бюджет', money, 'Сдача', money - product_weight * price_of_the_product)
 
-# print("ведите количство строк")
-# number_of_lines = int(input())
-# print("Купи конструктор!\n" * number_of_lines)
+print("ведите количство строк")
+number_of_lines = int(input())
+print("Купи конструктор!\n" * number_of_lines)
 
 print("Введите число строк")
 x = int(input())
